feab23ff1f8bd72b768b784f0ac2d92320b7a56d
oglavu <ognjenmirkovic032@gmail.com>
2024-05-26 09:58:07 -0700

    BoundBuffer implemented

---
+++ h/Buffer.hpp
@@ -0,0 +1,36 @@
+//
+// Created by os on 5/22/24.
+//
+
+#ifndef PROJEKAT_BUFFER_HPP
+#define PROJEKAT_BUFFER_HPP
+
+#include "_sem.hpp"
+#include "MemoryAllocator.h"
+
+
+class Buffer {
+public:
+    Buffer();
+    static Buffer* inBuffer;
+    static Buffer* outBuffer;
+
+    char getc();
+    void putc(char);
+
+    void* operator new(size_t sz);
+    void operator delete(void* p);
+
+private:
+
+
+    _sem* spaceAvailable;
+    _sem* itemAvailable;
+    static const uint64 SIZE = 128;
+    uint64 start = 0, end = 0;
+    char array[SIZE];
+
+ };
+
+
+#endif //PROJEKAT_BUFFER_HPP
---
+++ src/Buffer.cpp
@@ -0,0 +1,37 @@
+//
+// Created by os on 5/22/24.
+//
+
+#include "../h/Buffer.hpp"
+
+
+Buffer* Buffer::inBuffer = nullptr;
+Buffer* Buffer::outBuffer = nullptr;
+
+Buffer::Buffer() {
+    sem_open(&this->spaceAvailable, SIZE);
+    sem_open(&this->itemAvailable, 0);
+}
+
+char Buffer::getc() {
+    itemAvailable->wait();
+    char c = array[start];
+    start = (start+1) % SIZE;
+    spaceAvailable->signal();
+    return c;
+}
+
+void Buffer::putc(char c){
+    spaceAvailable->wait();
+    array[end] = c;
+    end = (end+1) % SIZE;
+    itemAvailable->signal();
+}
+
+void *Buffer::operator new(size_t sz) {
+    return MemoryAllocator::mem_alloc(sz);
+}
+
+void Buffer::operator delete(void *p) {
+    MemoryAllocator::mem_free(p);
+}
